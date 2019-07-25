d06b0395f572c8213f150e802b9a4955a871fb79
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-07-24 20:44:17 -0400

    added animation to the nav bar

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,4 @@
+// on screen animations using greensock/tweenmax
 TweenMax.to(".loader h1 .loader-svg", 3, {
     opacity: 0,
     y: -60,
@@ -10,15 +11,26 @@ TweenMax.to(".loader", 3, {
     ease: Expo.easeInOut
 });
 
-
-
 TweenMax.from(".title-svg", 2, {
-    delay: 2.4,
+    delay: 2.5,
     opacity: 0,
     y: 20,
     ease: Expo.easeInOut
 });
 
+TweenMax.staggerFrom("nav a", 1, {
+    delay: 3,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.3);
+
+
+
+
+
+
+// animated draw svg using drawsvg.js
 var $svg = $('.loader-svg').drawsvg({
     duration: 2000,
     reverse: false,
