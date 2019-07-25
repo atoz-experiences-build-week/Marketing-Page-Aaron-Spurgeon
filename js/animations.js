cb4cc90e38012e2ecb4a3f7fbb492d9ae48a1aee
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-07-24 20:38:36 -0400

    spent a lot of time debugging the load animation, got it up and running though!

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,27 @@
-TweenMax.to(".loader h1", 2, {
+TweenMax.to(".loader h1 .loader-svg", 3, {
     opacity: 0,
     y: -60,
     ease: Expo.easeInOut
 });
 
-TweenMax.to(".loader", 2, {
+TweenMax.to(".loader", 3, {
     delay: 1,
-    top: "-100%",
+    top: "-120%",
     ease: Expo.easeInOut
-})
+});
+
+
+
+TweenMax.from(".title-svg", 2, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+var $svg = $('.loader-svg').drawsvg({
+    duration: 2000,
+    reverse: false,
+});
+$svg.drawsvg('animate');
+
--- js/app.js
+++ js/app.js
@@ -1,8 +1,2 @@
 // SVG animation test
 
-var $svg = $('svg').drawsvg({
-    duration: 5000,
-    reverse: false,
-    stagger: 2000
-});
-$svg.drawsvg('animate');
