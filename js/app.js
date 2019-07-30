cae9ce335ea6582ca51b43e0d11538f85199ffe7
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-07-30 19:53:30 -0400

    getting started on implementing a vanilla js sticky nav bar

--- js/app.js
+++ js/app.js
@@ -15,4 +15,19 @@ var glide = new Glide('.glide', {
     }
   })
   
-  glide.mount()
+  glide.mount();
+
+  // STICKY NAVBAR USING VANILLA JAVASCRIPT
+
+  const nav = document.querySelector('.nav-bar');
+  const topOfNav = nav.offsetTop; //40
+
+  function fixNav () {
+    if (window.scrollY >= topOfNav) {
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+    }
+  }
+
+  window.addEventListener('scroll', fixNav);
